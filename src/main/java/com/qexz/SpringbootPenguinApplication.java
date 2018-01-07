35ba9b5c76886e4d390838cf5df8ef98eac0d9ec
gdufeZLYL <18027894043@163.com>
2018-01-07 19:55:56 +0800

    first commit

---
+++ src/main/java/com/qexz/SpringbootPenguinApplication.java
@@ -0,0 +1,12 @@
+package com.qexz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPenguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPenguinApplication.class, args);
+	}
+}
