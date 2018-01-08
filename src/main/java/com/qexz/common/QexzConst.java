a7d754ad75bc1bbcbe70d6c7f37597940e88a8db
gdufeZLYL <18027894043@163.com>
2018-01-08 12:01:35 +0800

    基本配置

---
+++ src/main/java/com/qexz/common/QexzConst.java
@@ -0,0 +1,7 @@
+package com.qexz.common;
+
+public class QexzConst {
+
+    //本地服务器-文件,图片所在位置
+    public static final String UPLOAD_FILE_PATH = "file:D:/IdeaProjects/data/springboot-penguin/upload/";
+}
