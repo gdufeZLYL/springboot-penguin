54039503141d1d2b4e2bf531cc0e3bc7ff10be12
gdufeZLYL <18027894043@163.com>
2018-01-08 11:31:16 +0800

    基本配置

--- src/main/java/com/qexz/Application.java
+++ src/main/java/com/qexz/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootPenguinApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPenguinApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
