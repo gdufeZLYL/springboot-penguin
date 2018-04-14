5178d33fb800b7e9ce00e55dd4c891d64a4a8643
gdufeZLYL <18027894043@163.com>
2018-04-14 23:42:22 +0800

    文件路径

--- src/main/java/com/qexz/common/QexzConst.java
+++ src/main/java/com/qexz/common/QexzConst.java
@@ -5,10 +5,13 @@ public class QexzConst {
     // Session
     public final static String CURRENT_ACCOUNT = "current_account";
 
-    //本地服务器-文件,图片所在位置
-    public static final String UPLOAD_FILE_PATH = "file:D:/IdeaProjects/data/springboot-penguin/upload/";
-    //本地服务器-图片上传位置
-    public static final String UPLOAD_FILE_IMAGE_PATH = "D:/IdeaProjects/data/springboot-penguin/upload/images/";
+    //本地服务器-文件,图片所在位置,图片上传位置
+//    public static final String UPLOAD_FILE_PATH = "file:D:/IdeaProjects/data/springboot-penguin/upload/";
+//    public static final String UPLOAD_FILE_IMAGE_PATH = "D:/IdeaProjects/data/springboot-penguin/upload/images/";
+
+    //云服务器-文件,图片所在位置,图片上传位置
+    public static final String UPLOAD_FILE_PATH = "file:/usr/java/data/springboot-penguin/upload/";
+    public static final String UPLOAD_FILE_IMAGE_PATH = "/usr/java/data/springboot-penguin/upload/images/";
 
     //默认头像url
     public static final String DEFAULT_AVATAR_IMG_URL = "headimg_placeholder.png";
